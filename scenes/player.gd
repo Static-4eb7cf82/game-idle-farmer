adbd7d4e8e3f8098d6d17db246f9da107cbf9611
Static-4eb7cf82 <Static.4eb7cf82@gmail.com>
2024-08-30 18:45:01 -0700

    allow spamming particles

--- scenes/player.gd
+++ scenes/player.gd
@@ -1,13 +1,11 @@
 extends Node2D
 # todo: Change the autoload from a scene to a script
 
-var particles_packed_scene := preload("res://scenes/generic_splash_particle.tscn")
+
 var click_particles : CPUParticles2D
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-    click_particles = particles_packed_scene.instantiate() as CPUParticles2D
-    click_particles.emitting = false
-    add_child(click_particles)
+    pass
     
 
 
@@ -20,7 +18,13 @@ func _unhandled_input(event: InputEvent) -> void:
     if event is InputEventMouseButton:
         var mouse_event := event as InputEventMouseButton
         if mouse_event.button_index == MOUSE_BUTTON_LEFT and mouse_event.pressed:
-            print("[Player] Mouse down at: ", get_global_mouse_position())
-            click_particles.position = get_global_mouse_position()
-            click_particles.emitting = true
+            spawn_particles_at_mouse()
             # get_viewport().set_input_as_handled()
+
+
+var particles_packed_scene := preload("res://scenes/generic_splash_particle.tscn")
+func spawn_particles_at_mouse() -> void:
+    click_particles = particles_packed_scene.instantiate() as CPUParticles2D
+    add_child(click_particles)
+    click_particles.position = get_global_mouse_position()
+    click_particles.emitting = true
