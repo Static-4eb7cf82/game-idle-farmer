ea597a75d9c1aa43fb276fca1f1848ca8bee2261
Static-4eb7cf82 <Static.4eb7cf82@gmail.com>
2024-09-08 17:06:51 -0700

    handle open close side menu with tween

---
+++ scenes/ui/side_menu/side_menu.gd
@@ -0,0 +1,40 @@
+extends Control
+
+var is_open := false
+const opened_position := Vector2(322, 196)
+const closed_position := Vector2(628, 196)
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+    close_side_menu()
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+    pass
+
+
+func _on_open_close_button_pressed() -> void:
+    toggle_open()
+
+
+func toggle_open() -> void:
+    if is_open:
+        close_side_menu()
+    else:
+        open_side_menu()
+
+
+func open_side_menu() -> void:
+    is_open = true
+    var tween := get_tree().create_tween()
+    tween.tween_property(self, "position", opened_position, 0.3).set_trans(Tween.TRANS_CUBIC)
+    ($HBoxContainer/OpenCloseButton as Button).text = ">"
+    # position = opened_position
+
+
+func close_side_menu() -> void:
+    is_open = false
+    var tween := get_tree().create_tween()
+    tween.tween_property(self, "position", closed_position, 0.3).set_trans(Tween.TRANS_CUBIC)
+    ($HBoxContainer/OpenCloseButton as Button).text = "<"
