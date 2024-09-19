4c9ad64a3cb18f22e1b288b21b9d3b68c028fe98
Static-4eb7cf82 <Static.4eb7cf82@gmail.com>
2024-09-18 20:30:46 -0700

    set coin cost tooltip pluralism dynamically

--- scenes/ui/plant_menu/crop_button.gd
+++ scenes/ui/plant_menu/crop_button.gd
@@ -13,7 +13,8 @@ func _ready() -> void:
     $Button/MarginContainer/HBoxContainer/HBoxContainer/CropCostLabel.text = str(Global.CROP_COST[crop_resource.type])
     $Button.tooltip_text = Global.CROP_NAMES[crop_resource.type]
     $Button.tooltip_text += "\nTime: " + str(Global.CROP_GROWTH_DURATION[crop_resource.type]) + " seconds"
-    $Button.tooltip_text += "\nCost: " + str(Global.CROP_COST[crop_resource.type]) + " coins"
+    var coins_cost_str := " coins" if Global.CROP_COST[crop_resource.type] > 1 else " coin"
+    $Button.tooltip_text += "\nCost: " + str(Global.CROP_COST[crop_resource.type]) + coins_cost_str
     $Button.tooltip_text += "\nReward: " + str(Global.CROP_REWARD[crop_resource.type]) + " coins"
 
 
--- scenes/ui/plant_menu/till_button.gd
+++ scenes/ui/plant_menu/till_button.gd
@@ -6,6 +6,9 @@ signal button_pressed()
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
     $Button/MarginContainer/HBoxContainer/HBoxContainer/CoinCostLabel.text = str(Global.settings.till_cost)
+    $Button.tooltip_text = "Till soil"
+    var coins_cost_str := " coins" if Global.settings.till_cost > 1 else " coin"
+    $Button.tooltip_text += "\nCost: " + str(Global.settings.till_cost) + coins_cost_str
 
 
 func _on_button_pressed() -> void:
