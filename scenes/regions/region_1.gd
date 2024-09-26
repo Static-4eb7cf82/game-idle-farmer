2f7670e9c18b28637124d7a885d85e4697121f64
Static-4eb7cf82 <Static.4eb7cf82@gmail.com>
2024-09-25 18:15:12 -0700

    redesign region 1 layout

--- scenes/regions/region_1.gd
+++ scenes/regions/region_1.gd
@@ -3,28 +3,29 @@ extends Region
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
     super()
+    
+    set_initial_state()
+
+
+func set_initial_state() -> void:
 
     # Add the first cat worker
     add_cat_worker(Vector2(200, 200))
     # add_cat_worker(Vector2(216, 200))
-    add_storage_container(Vector2i(15, 4))
-    # add_storage_container(Vector2i(10, 4))
-    add_water_well(Vector2i(23, 9))
-    
-    debug_seed_tilled_soil()
+    # add_storage_container(Vector2i(24, 9))
+    add_storage_container(Vector2i(8, 11))
+    add_water_well(Vector2i(5, 15))
 
-
-func debug_seed_tilled_soil() -> void:
-    place_tilled_soil_at_coords(Vector2i(14, 10))
-    place_tilled_soil_at_coords(Vector2i(15, 10))
-    place_tilled_soil_at_coords(Vector2i(16, 10))
-    place_tilled_soil_at_coords(Vector2i(17, 10))
-    place_tilled_soil_at_coords(Vector2i(14, 11))
-    place_tilled_soil_at_coords(Vector2i(15, 11))
-    place_tilled_soil_at_coords(Vector2i(16, 11))
-    place_tilled_soil_at_coords(Vector2i(17, 11))
-    place_tilled_soil_at_coords(Vector2i(14, 12))
-    place_tilled_soil_at_coords(Vector2i(15, 12))
-    place_tilled_soil_at_coords(Vector2i(16, 12))
-    place_tilled_soil_at_coords(Vector2i(17, 12))
+    place_tilled_soil_at_coords(Vector2i(13, 13))
+    place_tilled_soil_at_coords(Vector2i(14, 13))
+    place_tilled_soil_at_coords(Vector2i(15, 13))
+    place_tilled_soil_at_coords(Vector2i(16, 13))
+    place_tilled_soil_at_coords(Vector2i(13, 14))
+    place_tilled_soil_at_coords(Vector2i(14, 14))
+    place_tilled_soil_at_coords(Vector2i(15, 14))
+    place_tilled_soil_at_coords(Vector2i(16, 14))
+    place_tilled_soil_at_coords(Vector2i(13, 15))
+    place_tilled_soil_at_coords(Vector2i(14, 15))
+    place_tilled_soil_at_coords(Vector2i(15, 15))
+    place_tilled_soil_at_coords(Vector2i(16, 15))
 
